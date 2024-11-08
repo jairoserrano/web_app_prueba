a57eed3c4a7a11c01ee82a2bbcddd81465cb8af6
Jairo Serrano <jairo.serrano@gmail.com>
2024-11-07 20:52:59 -0500

    actualización de validación

--- js/libreria.js
+++ js/libreria.js
@@ -1,4 +1,35 @@
 
 $(document).ready(function(){
-    $("#inputName").text("Hola Mundo");
+    //$("#inputName").val("Hola Mundo");
+    //$("#parrafoCabecera").text("Hola Mundo");
+    //$("#mensaje").html("<h2>Hola Mundo</h2>");
+
+    $("#mensajeName").hide();
+
+    $("#inputAddress").on("blur", function(){
+        if ($("#inputAddress").val().length > 10){
+            console.log("Dirección correcta");
+        }else{
+            console.log("Dirección incorrecta");
+        }
+    });
+
+    $("#inputEmail").on("blur", function(){
+        if ($("#inputEmail").val().indexOf("@") == -1){
+            console.log("Email INcorrecto");
+        }else{
+            console.log("Email correcto");
+        }
+    });
+
+    $("#enviar").click(function(){
+        let nombre = $("#inputName").val();
+        if (nombre.length > 3) {
+            console.log("Nombre: " + nombre + " es correcto.");
+            $("#mensajeName").hide();
+        } else {
+            $("#mensajeName").show();
+        }
+    });
+
 });
